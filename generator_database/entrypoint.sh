25bb038caabe260eac0324195909f3fa0ef3c498
devcxl <64475363+devcxl@users.noreply.github.com>
2025-05-05 14:42:51 +0800

    add gpg-passphrase

--- generator_database/entrypoint.sh
+++ generator_database/entrypoint.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-echo "$GPG_PRIVATE_KEY" | gpg --import
+# 杀掉可能冲突的 GPG 代理
+gpgconf --kill gpg-agent
 
-cd $PACKAGE_PATH
+# 导入私钥（如果设置了密码，必须提供）
+echo "$GPG_PRIVATE_KEY" | gpg --batch --pinentry-mode loopback --passphrase "$GPG_PASSPHRASE" --import
 
-packages=("*.pkg.tar.zst")
-for name in $packages; do
-    gpg --detach-sig --yes $name
+# 进入包目录
+cd "$PACKAGE_PATH"
+
+# 对所有 .pkg.tar.zst 文件签名
+for name in *.pkg.tar.zst; do
+    gpg --batch --pinentry-mode loopback --passphrase "$GPG_PASSPHRASE" --detach-sig --yes "$name"
 done
+
+# 更新仓库数据库并签名
 repo-add --verify --sign "$DATABASE.db.tar.gz" *.pkg.tar.zst
